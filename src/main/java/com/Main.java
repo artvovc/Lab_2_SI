df2ded5c2e083d5cde4be40a5116c8bcaaf8de70
Vovc Artiom <vovc.artiom@gmail.com>
2016-09-23 16:03:04 +0300

    first RSA commit

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int iBitSize = 1024;
+        try
+        {
+            RSA r = new RSA(iBitSize);
+            System.out.println("Enter message: ");
+            System.out.println();
+            String input = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+            String sHexCipherText = r.encryptPlainStrToHex(input);
+            String sPlainText = r.decryptHexCipherToPlainMsg(sHexCipherText);
+            System.out.println();
+            System.out.println("CipherText: " + sHexCipherText);
+            System.out.println("PlainText/Original message: "+sPlainText);
+            System.out.println();
+        } catch (Exception e1) {
+        }
+
+    }
+}
