73c2b97986a595866bdcc92fe1bb73694a18414f
Vovc Artiom <vovc.artiom@gmail.com>
2016-10-25 23:25:16 +0300

    Reviewed (1)

--- src/main/java/com/RSA.java
+++ src/main/java/com/RSA.java
@@ -4,13 +4,12 @@ import java.math.BigInteger;
 import java.util.Random;
 
 class RSA {
-    private int TextSize;
     private BigInteger PrivateKey;
     private BigInteger PublicKey;
     private BigInteger ProdPQ;
+    private int TextSize;
 
-    RSA(int TextSize)
-    {
+    RSA(int TextSize) {
         this.TextSize = TextSize;
         BigInteger primeP = BigInteger.probablePrime(this.TextSize, new Random());
         BigInteger primeQ = BigInteger.probablePrime(this.TextSize, new Random());
@@ -18,9 +17,9 @@ class RSA {
         BigInteger fi = primeP.subtract(BigInteger.ONE).multiply(primeQ.subtract(BigInteger.ONE));
         do {
             PublicKey = new BigInteger(this.TextSize, new Random());
-            if (    (PublicKey.compareTo(fi) == -1) &&
+            if ((PublicKey.compareTo(fi) == -1) &&
                     (PublicKey.compareTo(BigInteger.ONE) == 1) &&
-                    (PublicKey.gcd(fi).compareTo(BigInteger.ONE) == 0)  )
+                    (PublicKey.gcd(fi).compareTo(BigInteger.ONE) == 0))
                 break;
         } while (true);
         PrivateKey = PublicKey.modInverse(fi);
@@ -47,57 +46,46 @@ class RSA {
     }
 
     private String encrypt(String hexString, BigInteger N, BigInteger e) {
-        if (hexString.length() == 0)
-            return null;
         int stringLenght = TextSize / 2;
-        if (stringLenght <= hexString.length()) {
-            String returnString = "";
-            String pathString;
-            int startIndex = 0;
-            int finalIndex = stringLenght - 1;
-            while (startIndex < hexString.length()) {
-                if (finalIndex < hexString.length()) {
-                    pathString = (new BigInteger(hexString.substring(startIndex, finalIndex), 16)).modPow(e, N).toString(16);
-                    startIndex = finalIndex;
-                    finalIndex += (stringLenght - 1);
-                } else {
-                    pathString = (new BigInteger(hexString.substring(startIndex), 16)).modPow(e, N).toString(16);
-                    startIndex = hexString.length();
-                }
-                if (pathString.length() < stringLenght) {
-                    int iLen = stringLenght - pathString.length();
-                    for (int k = 0; k < iLen; k++)
-                        pathString = "0" + pathString;
-                }
-                returnString += pathString;
+        String returnString = "";
+        String pathString;
+        int startIndex = 0;
+        int finalIndex = stringLenght;
+        while (startIndex < hexString.length()) {
+            if (finalIndex < hexString.length()) {
+                pathString = (new BigInteger(hexString.substring(startIndex, finalIndex), 16)).modPow(e, N).toString(16);
+                startIndex = finalIndex;
+                finalIndex += stringLenght;
+            } else {
+                pathString = (new BigInteger(hexString.substring(startIndex), 16)).modPow(e, N).toString(16);
+                startIndex=hexString.length();
             }
-            return returnString;
-        } else
-            return (new BigInteger(hexString, 16)).modPow(e, N).toString(16);
+            returnString += pathString;
+        }
+        return returnString;
+
     }
 
     private String decrypt(String hexString, BigInteger N, BigInteger d) {
-        if (hexString.length() == 0)
-            return null;
         int stringLenght = TextSize / 2;
-        if (stringLenght < hexString.length()) {
-            String returnString = "";
-            int startIndex = 0;
-            int finalIndex = stringLenght;
-            while (startIndex < hexString.length()) {
-                if (finalIndex < hexString.length()) {
-                    returnString += (new BigInteger(hexString.substring(startIndex, finalIndex), 16)).modPow(d, N).toString(16);
-                    startIndex = finalIndex;
-                    finalIndex += stringLenght;
-                } else {
-                    returnString += (new BigInteger(hexString.substring(startIndex), 16)).modPow(d, N).toString(16);
-                    break;
-                }
-            }
-            return returnString;
-        } else
-            return (new BigInteger(hexString, 16)).modPow(d, N).toString(16);
-    }
 
+        String returnString = "";
+        String pathString;
+        int startIndex = 0;
+        int finalIndex = stringLenght;
+        while (startIndex < hexString.length()) {
+            if (finalIndex < hexString.length()) {
+                pathString = (new BigInteger(hexString.substring(startIndex, finalIndex), 16)).modPow(d, N).toString(16);
+                startIndex = finalIndex;
+                finalIndex += stringLenght;
+            } else {
+                pathString = (new BigInteger(hexString.substring(startIndex), 16)).modPow(d, N).toString(16);
+                startIndex=hexString.length();
+            }
+            returnString += pathString;
+        }
+        return returnString;
+
+    }
 
 }
